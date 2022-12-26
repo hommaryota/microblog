295b728882d384d68e69fc599645e34dd7e2b1c8
Honma Ryota <kabuprivate@gmail.com>
2022-12-26 18:18:27 +0900

    ブログトップページ作成

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,59 @@
 import Layout from '../components/Layout';
+import utilStyles from "../styles/utils.module.css";
+import styles from "../styles/home.module.css";
+import { getPostsData } from '../lib/post';
 
 import { siteTitle } from '../components/Layout';
-export default function Home() {
+import Link from 'next/link';
+
+
+//SGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+// //SSRの場合
+// export async function getSeverSideProps(context) {
+//   return {
+//     props: {
+
+//     }
+//   }
+// }
+
+export default function Home({ allPostsData }) {
   return (
     <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>私はフルスタックエンジニアです/Udemy講師として各同しています/好きな言語はjavascriptです</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={`${thumbnail}`} alt='サムネイル' className={styles.thumbnailImage} />
+              </Link>
+              <Link href={`/posts/${id}`}>
+                <span className={utilStyles.boldText}>{title}</span>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                {date}
+              </small>
+            </article>
+          ))}
+        </div>
+      </section>
+
       <h1>
         {siteTitle}
       </h1>
